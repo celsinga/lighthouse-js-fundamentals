683898d19c78221475f63040aa19d16acb5dd7d0
Colin <colinelsinga@gmail.com>
2020-11-08 17:32:44 +0000

    Committing recent work

---
+++ new.js
@@ -0,0 +1,57 @@
+// const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']
+
+/* const finalPosition = function (moves) {
+  let one = 0;
+  let two = 0;
+  for (let i = 0; i < moves.length; i++) {
+    if (moves[i] === 'north') {
+      two += 1;
+    } else if (moves[i] === 'west') {
+      one -= 1;
+    } else if (moves[i] === 'east') {
+      one += 1;
+    }
+  }  
+  const position = [one, two];
+  return position
+}
+
+console.log(finalPosition(moves))
+
+*/
+
+// const ageCalculator = function(name, yearOfBirth, currentYear) {
+ // let calc = currentYear - yearOfBirth;
+  //let statement = name + " is " + calc + " years old."
+ // return statement
+//}
+
+// let howManyHundreds = function (num) {
+//  let input = num / 100;
+//  let output = Math.floor(input)
+//  return output
+//}
+
+/* const calculateRectangleArea = function (length, width) {
+  let result = length * width;
+  if (result >= 0) {
+    return result;
+  }
+}
+
+const calculateTriangleArea = function (base, height) {
+  let result = base * height / 2;
+  if (result >= 0) {
+    return result;
+  }
+}
+
+const calculateCircleArea = function (radius) {
+  let result = Math.PI * radius * radius;
+  if (result >= 0) {
+    return result;
+  }
+}
+
+*/ 
+
